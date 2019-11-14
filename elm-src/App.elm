824506fb652865773c55cde18ed1c8743567ea07
Anthony Scopatz <scopatz@gmail.com>
2019-11-14 18:37:51 -0500

    much reduced version

--- elm-src/App.elm
+++ elm-src/App.elm
@@ -7,30 +7,24 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Decode
+import Json.Decode exposing (string, field, maybe, bool)
 import Json.Decode as Decode
 import Json.Encode as Encode
+import Json.Decode.Pipeline exposing (optional, optionalAt, required, requiredAt)
 
 -- Types
-type alias ServerResult = {}
-
-type alias InputFlags =
-    { placeholder: String
-    , serverUrl: String
-    }
 
 type alias Model =
     { placeholder : String
     , serverUrl : String
-    , buttonClicked : Bool
-    , response : Maybe ServerResult
-    , error : Maybe Http.Error
+    , setup: String
+    , started : Bool
+    , flagsError : String
     }
 
 type Msg
     = NoOp
     | ButtonClicked
-    | Response (Result Http.Error ServerResult)
 
 
 -- UPDATE
@@ -43,13 +37,7 @@ update msg model =
             ( model, Cmd.none )
 
         ButtonClicked ->
-            ( { model | buttonClicked = True }, Cmd.none )
-
-        Response (Ok response) ->
-            ( { model | error = Nothing, response = Just response }, Cmd.none )
-
-        Response (Err error) ->
-            ( { model | error = Just error, response = Nothing }, Cmd.none )
+            ( { model | started = True }, Cmd.none )
 
 
 
@@ -62,65 +50,8 @@ update msg model =
 
 -- VIEWS
 
-
-
-viewResponse : ServerResult -> Html Msg
-viewResponse response =
-    div [ class "response-container" ]
-        [ h2 [] [ text "Results" ]
-        , div []
-            [ text "here" ]
-        ]
-
-
-viewError : Http.Error -> Html Msg
-viewError error =
-    div [ class "error-container" ]
-        [ h2 [] [ text "Search Errors" ]
-        , div [] (case error of
-            Http.BadUrl url ->
-                [ text ("Bad URL: " ++ url)]
-            Http.Timeout ->
-                [ text "Network timeout" ]
-            Http.NetworkError ->
-                [ text "Network error" ]
-            Http.BadStatus code ->
-                [ text ("Bad status: " ++ String.fromInt code) ]
-            Http.BadBody body ->
-                [ text body ]
-        )
-        ]
-
-
-viewUtils :
-    { a | error : Maybe Http.Error, response : Maybe ServerResult }
-    -> ({ a | error : Maybe Http.Error, response : Maybe ServerResult } -> Html Msg)
-    -> Html Msg
-viewUtils model viewF =
-    div []
-        [ viewF model
-        , case model.response of
-            Just response ->
-                viewResponse response
-
-            Nothing ->
-                text ""
-        , case model.error of
-            Just error ->
-                viewError error
-
-            Nothing ->
-                text ""
-        ]
-
-
 view : Model -> Html Msg
 view model =
-    viewUtils model viewForm
-
-
-viewForm : Model -> Html Msg
-viewForm model =
     Html.div
         [ class "runthis-container" ]
         [ button
@@ -128,7 +59,11 @@ viewForm model =
             [ text "Run This" ]
         , br [] []
         , div [ class "runthis-contents" ]
-            [ if model.buttonClicked then
+            [ if not (String.isEmpty model.flagsError) then
+                p [] [ h2 [] [text "JSON Flags Error"]
+                     , text model.flagsError
+                     ]
+              else if model.started then
                 iframe [ src (model.serverUrl ++ "?setup=print"), style "width" "100%", height 400 ] []
               else
                 p [] [ text model.placeholder ]
@@ -136,21 +71,34 @@ viewForm model =
         ]
 
 
-init : InputFlags -> ( Model, Cmd Msg )
-init flags =
-    ( { placeholder = flags.placeholder
-      , serverUrl = flags.serverUrl
-      , buttonClicked = False
-      , response = Nothing
-      , error = Nothing
-      }
-    , Cmd.none
-    )
-
 
 -- MAIN
 
-main : Program InputFlags Model Msg
+modelDecoder : Decode.Decoder Model
+modelDecoder =
+    Decode.succeed Model
+        |> required "placeholder" string
+        |> required "serverUrl" string
+        |> required "setup" string
+        |> optional "started" bool False
+        |> optional "flagsError" string ""
+
+init : Decode.Value -> ( Model, Cmd Msg )
+init flags =
+    ( case (Decode.decodeValue modelDecoder flags) of
+        Ok value ->
+            value
+        Err error ->
+            { placeholder = ""
+            , serverUrl = ""
+            , setup = ""
+            , started = False
+            , flagsError = Decode.errorToString error
+            }
+    , Cmd.none
+    )
+
+main : Program Decode.Value Model Msg
 main =
   Browser.element
         { init = init
